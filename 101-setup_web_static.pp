007f897487ea8d1a7980c27c90abd217796ba9c2
Ahmerd196 <ahmerd196@gmail.com>
2023-07-06 11:47:18 -0700

    AirBnB task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,50 +1,72 @@
-file {
-  '/data':
-    ensure => 'directory',
-    owner  => 'ubuntu',
-    group  => 'ubuntu';
+file { '/tmp/':
+  ensure => 'directory',
+}
 
-  '/data/web_static':
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root';
+file { '/tmp/holberton':
+  ensure  => 'file',
+  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n",
+}
 
-  '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test',
-    owner  => 'root',
-    group  => 'root',
-    require => File['/data/web_static'];
+file { '/etc/puppet/':
+  ensure => 'directory',
+}
 
-  '/data/web_static/releases':
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    require => File['/data/web_static'];
+file { '/etc/puppet/holberton/':
+  ensure => 'directory',
+}
 
-  '/data/web_static/shared':
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    require => File['/data/web_static'];
-  
-  '/data/web_static/releases/test':
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    require => File['/data/web_static/releases'];
-  
-  '/data/web_static/current/index.html':
-    ensure  => 'file',
-    content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>',
-    owner   => 'root',
-    group   => 'root',
-    require => File['/data/web_static/releases/test'];
+file { '/etc/puppet/holberton/holberton':
+  ensure  => 'file',
+  content => "User-agent: *\nDisallow: /",
+}
+
+package { 'nginx':
+  ensure => 'installed',
 }
 
 service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  require => File['/data/web_static/current/index.html'];
+  ensure    => 'running',
+  enable    => true,
+  hasstatus => true,
+  require   => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\n\tlocation /redirect_me {\n\t\treturn 301 http://mydomain.tech/hbnb_static/index.html;\n\t}\n\n\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}\n}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+file { '/data/':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases/':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/shared/':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases/test/':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test',
+  require => File['/data/web_static/releases/test/'],
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'file',
+  content => "/tmp/holberton",
+  require => File['/tmp/holberton'],
 }
 
